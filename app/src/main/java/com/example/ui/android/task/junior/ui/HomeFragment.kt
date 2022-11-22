142386bdf5e7eeb7f0a97ca33135efe3f2e3e184
AliJonMadaminov <aliufc2004@gmail.com>
2022-11-22 12:35:15 +0500

    camera-centered marker moving done
    - bottom sheet almost done

--- app/src/main/java/com/example/ui/android/task/junior/ui/HomeFragment.kt
+++ app/src/main/java/com/example/ui/android/task/junior/ui/HomeFragment.kt
@@ -1,34 +1,51 @@
 package com.example.ui.android.task.junior.ui
 
-import androidx.fragment.app.Fragment
-
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.graphics.drawable.toBitmap
+import androidx.fragment.app.Fragment
 import com.example.ui.android.task.junior.R
-
+import com.example.ui.android.task.junior.databinding.FragmentHomeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap.OnCameraMoveListener
+import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
+
         val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val markerDrawable = getDrawable(requireContext(), R.drawable.blue_map_pin)
+        val markerIcon = markerDrawable?.toBitmap(markerDrawable.intrinsicWidth, markerDrawable.intrinsicHeight)
+        var marker: Marker? = googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+                .icon(BitmapDescriptorFactory.fromBitmap(markerIcon!!))
+        )
+
+        googleMap.setOnCameraMoveListener {
+            if (marker != null) {
+                marker.position = googleMap.cameraPosition.target
+            }
+        }
+
     }
 
     override fun onCreateView(
@@ -36,7 +53,9 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,4 +63,5 @@ class HomeFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
 }
--- app/src/main/java/com/example/ui/android/task/junior/ui/TripDetailsFragment.kt
+++ app/src/main/java/com/example/ui/android/task/junior/ui/TripDetailsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ui.android.task.junior.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
